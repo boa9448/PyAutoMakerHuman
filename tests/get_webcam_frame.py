a9a7501ec01b0654f6d40e0c40eddc83b3350dee
WDW <boa3465@gmail.com>
2021-07-13 00:07:35 +0900

    필요없는 파일 삭제

--- tests/get_webcam_frame.py
+++ tests/get_webcam_frame.py
@@ -3,15 +3,14 @@ import cv2
 import numpy as np
 import time
 import requests as rq
+import PyAutoMakerHuman as pamh
 
 save_path = "temp"
 target_url = "http://172.30.1.55:8080/shot.jpg"
 
 start = time.time()
 while True:
-    data = rq.get(target_url)
-    data = np.frombuffer(data.content, dtype=np.byte)
-    frame = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
+    frame = pamh.get_webcam_frame(target_url)
 
     file_name = os.path.join(save_path, f"frame_{time.time()}.jpg")
     cv2.imwrite(file_name, frame)
