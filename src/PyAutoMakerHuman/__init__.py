45f78b24027d3587d9ea9575c43cd5212fdfeaa8
WDW <boa3465@gmail.com>
2022-02-12 16:57:17 +0900

    모델 경로 변경

--- src/PyAutoMakerHuman/__init__.py
+++ src/PyAutoMakerHuman/__init__.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 cur_dir = os.path.dirname(__file__)
-model_dir = os.path.join(cur_dir, "..", "..", "models_new")
+model_dir = os.path.join(cur_dir, "..", "..", "models")
 model_dir = os.path.abspath(model_dir)
