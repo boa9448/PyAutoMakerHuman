1fbd5ab2126ce74cf579d045994a2fde558f5ab6
WDW <boa3465@gmail.com>
2022-02-13 14:45:21 +0900

    gui 코드와 처리 스레드의 코드 분리 작업중  그리기 시그널, 정답 처리 시그널, 방향 처리 시그널 작성  예외 작성

--- src/PyAutoMakerHuman/gui/proc.py
+++ src/PyAutoMakerHuman/gui/proc.py
@@ -1,15 +1,75 @@
+import time
 from threading import Thread, Event, Lock
 from typing import Callable
 
 import cv2
 import numpy as np
-from PySide6.QtCore import Slot, Signal
+from PySide6.QtCore import QObject, Slot, Signal
+
+from .exception import FrameException
+
+
+class FrontDrawSignal(QObject):
+    sig = Signal(np.ndarray)
+
+    def send(self, img : np.ndarray) -> None:
+        self.sig.emit(img)
+
+
+ANSWER_FAIL = 0
+ANSWER_PROCESSING = 1
+ANSWER_SUCCESS = 2
+class AnswerSignal(QObject):
+    sig = Signal(int)
+
+    def fail(self) -> None:
+        self.sig.emit(ANSWER_FAIL)
+
+    def processing(self) -> None:
+        self.sig.emit(ANSWER_PROCESSING)
+
+    def success(self) -> None:
+        self.sig.emit(ANSWER_SUCCESS)
+
+
+DIRECTION_NONE = 0
+DIRECTION_LEFT = 1
+DIRECTION_RIGHT = 2
+class DirectionSignal(QObject):
+    sig = Signal(int)
+
+    def stop(self) -> None:
+        self.sig.emit(DIRECTION_NONE)
+
+    def left(self) -> None:
+        self.sig.emit(DIRECTION_LEFT)
+
+    def right(self) -> None:
+        self.sig.emit(DIRECTION_RIGHT)
+
 
 class WorkThread(Thread):
+    FRAME_READ_TIMEOUT = 2
+
     def __init__(self, cameras : tuple[cv2.VideoCapture, cv2.VideoCapture]
-                    , draw_signal_handler : Callable):
+                    , front_draw_handler : Callable, answer_handler : Callable, direction_handler : Callable):
         super().__init__()
+        # 이벤트, 락
         self._exit_event = Event()
+
+        # 카메라
+        self._front_camera = cameras[0]
+        self._side_camera = cameras[1]
+
+        #시그널
+        self._front_draw_signal = FrontDrawSignal()
+        self._front_draw_signal.sig.connect(front_draw_handler)
+
+        self._answer_signal = AnswerSignal()
+        self._answer_signal.sig.connect(answer_handler)
+
+        self._direction_signal = DirectionSignal()
+        self._direction_signal.sig.connect(direction_handler)
 
     def exit(self) -> None:
         self._exit_event.set()
@@ -17,6 +77,23 @@ class WorkThread(Thread):
     def join(self, timeout: float or None = ...) -> None:
         self.exit()
         return super().join(timeout)
+
+    def get_frame(self, target_camera : cv2.VideoCapture) -> np.ndarray:
+        start_time = time.time()
+        while time.time() - start_time > self.FRAME_READ_TIMEOUT:
+            success, frame = target_camera.read()
+            if success:
+                return frame
+        
+        raise FrameException("프레임을 가져오는데 실패했습니다")
+
+    @property
+    def front_frame(self) -> np.ndarray:
+        self.get_frame(self._front_camera)
+
+    @property
+    def side_frame(self) -> np.ndarray:
+        self.get_frame(self._side_camera)
 
     def run(self) -> None:
         pass
