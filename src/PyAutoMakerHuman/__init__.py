3625d6575ba66702ec966aed25c4259aad7f4152
WDW <boa3465@gmail.com>
2022-02-16 17:18:12 +0900

    사소한 수정

--- src/PyAutoMakerHuman/__init__.py
+++ src/PyAutoMakerHuman/__init__.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 cur_dir = os.path.dirname(__file__)
-model_dir = os.path.join(cur_dir, "..", "..", "models")
-model_dir = os.path.abspath(model_dir)
+models_dir = os.path.join(cur_dir, "..", "..", "models")
+models_dir = os.path.abspath(models_dir)
