b59d9ad6dedff1cd569edcc3264759a1f9e391c0
WDW <boa3465@gmail.com>
2022-02-17 14:26:34 +0900

    예외 세분화

--- src/PyAutoMakerHuman/gui/exception.py
+++ src/PyAutoMakerHuman/gui/exception.py
@@ -1,3 +1,18 @@
 class FrameException(Exception):
     def __init__(self, message : str):
         super().__init__(message)
+
+
+class ExitException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class StopException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class DataModifyExecption(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
