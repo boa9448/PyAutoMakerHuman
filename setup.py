9bd8cae54cfa70c206a0dcc3b23c8f06d745d87f
WDW <boa3465@gmail.com>
2022-02-26 14:45:24 +0900

    패키지 데이터를 제대로 포함하도록 수정

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
+import os
 import setuptools
+
+
+def package_files(package_dir : str, data_dir : str) -> list:
+    paths = []
+    for (path, directories, filenames) in os.walk(os.path.join(package_dir, data_dir)):
+        for filename in filenames:
+            file_path = os.path.join(path, filename)
+            file_path = file_path.replace(package_dir, "")
+            paths.append(file_path)
+    return paths
+extra_files = package_files("src/PyAutoMakerHuman/", "gui/datas")
 
 with open("README.md", "rt", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
     name="PyAutoMakerHuman",
-    version="1.2.1",
+    version="2.0.0",
     author="WDW",
     author_email="boa3465@gmail.com",
     description="얼굴, 포즈, 손의 랜드마크를 쉽게 가져오기위한 모듈",
@@ -23,7 +36,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    package_data={"PyAutoMakerHuman": ["models/*", "gui/datas/*"]},
+    package_data={"PyAutoMakerHuman": ["models/**/*", *extra_files]},
 
     install_requires = ["opencv-contrib-python", "imutils", "scikit-learn"
                         ,"requests", "mediapipe", "PySide6", "qt-material"],
